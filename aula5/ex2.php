1925d12083f5531c083dbba6becd062719c6a5c7
nataly <tinarellinataly@gmail.com>
2022-03-23 11:01:31 -0300

    comando if else

---
+++ aula5/ex2.php
@@ -0,0 +1,22 @@
+<!-- 
+    COMANDOS CONDICIONAIS
+
+1) if-else
+2) switch-case
+
+-->
+
+<?php
+
+    $n1 = 7;
+    $n2 = 8;
+
+    $media = ($n1 + $n2)/2;
+
+    if($media >= 7)
+        echo "Você foi aprovado";
+    
+    else
+        echo "Você foi reprovado";
+
+?>
