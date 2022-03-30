3846d050facc97ded43b59ecf59c4ffd2bd1d6c9
nataly <tinarellinataly@gmail.com>
2022-03-30 09:18:48 -0300

    manipulação de datas

---
+++ aula6/exemploData.php
@@ -0,0 +1,6 @@
+<?php
+    //imprime a data do servidor
+    echo "Data atual: ".date('d/m/y')."<br>"; 
+    echo "Data atual: ".date('d/M/y')."<br>"; //mês abreviado
+    echo "Data atual: ".date('d/m/y H:i:s'); //mostrar data e hora
+?>
