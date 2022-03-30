b244fa4ba4529f46f6b292920c8595a4e7c9f6ba
nataly <tinarellinataly@gmail.com>
2022-03-30 08:57:56 -0300

    função trim

---
+++ aula6/teoria1.php
@@ -0,0 +1,19 @@
+<!--
+    MANIPULAÇÃO DE DATA
+
+    Para mostrar um caminho data e hora, usa-se:
+    echo.date('parametro1/parametro2/parametro3');
+
+    MANIPULAÇÃO DE STRINGS
+
+    Função TRIM()
+    obtém a quantidade de caracteres de uma String, eliminando os espaços iniciais e finais
+-->
+<?php
+    //exemplo Função TRIM()
+    $palavra = "Ciência da computação ";
+    echo "A variável contém " .strlen($palavra)." caracteres"."<br>";
+    
+    $palavra=trim($palavra);
+    echo "A variável agora contém: ".strlen($palavra)."caracteres.";
+?>
