2f9e1d48f2a175c0bc9758162225d36bd586b441
nataly <tinarellinataly@gmail.com>
2022-04-04 08:23:47 -0300

    for

---
+++ lista2/exer3.php
@@ -0,0 +1,10 @@
+<?php
+/*Um número é, por definição, primo se ele não tem divisores,
+exceto 1 e ele próprio. Preparar um script PHP para verificar
+números entre 0 e 200 e determinar se cada um dele é primo ou
+não.*/
+
+for($i=0;$i<=200;$i++){
+    if($num%)
+}
+?>
