3ef67780f26a464aab6597be8c0b2b36296b030e
naataly <tinarellinataly@gmail.com>
2022-04-06 08:50:19 -0300

    exer 3

--- lista2/exer3.php
+++ lista2/exer3.php
@@ -4,7 +4,27 @@ exceto 1 e ele próprio. Preparar um script PHP para verificar
 números entre 0 e 200 e determinar se cada um dele é primo ou
 não.*/
 
-for($i=0;$i<=200;$i++){
-    if($num%)
-}
+    $num;
+    $divisor;
+    
+    for($i=0;$i<=200;$i++){
+        while ($num>=$divisor){
+            $resto = $num%$divisor;
+
+            if($resto==0){
+                $cont=0;
+                $cont++;
+            }
+        
+        }
+
+        if($cont>2){
+            echo "O número não é primo.";
+        }
+        else{
+            echo "O número é primo";
+        }
+    }
+        
+
 ?>
