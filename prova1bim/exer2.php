37be6b28df1ad255e9781f9c11116c3501f8a6fd
nataly <tinarellinataly@gmail.com>
2022-04-13 08:52:39 -0300

    estrutura de repetição

---
+++ prova1bim/exer2.php
@@ -0,0 +1,16 @@
+<?php
+/*Efetue um script PHP que a partir de dois valores quaisquer e efetue
+sua multiplicação e apresente o resultado utilizando para isso apenas o operador “+”, visto que:
+• (3 * 5) = 5 + 5 + 5
+• (4 * 12) = 12 + 12 + 12 + 12*/
+
+//declaração de variáveis
+$fator1=2;
+$fator2=3;
+
+for($i=0; $i<=$fator2; $i++)
+    echo $fator1, " + ";
+
+
+
+?>
