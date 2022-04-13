fbf1b436bbf12f77be0686bf7eab40a16cd10e55
nataly <tinarellinataly@gmail.com>
2022-04-13 16:02:58 -0300

    exercicios

--- prova1bim/exer4/exer4.php
+++ prova1bim/exer4/exer4.php
@@ -4,8 +4,9 @@ $saldo = $_GET["saldo"];
 $nome = $_GET["nome"];
 $cpf = $_GET["cpf"];
 $rg = $_GET["rg"];
+$sexo = $_GET["sexo"];
 
-echo "Dados: <br> Nome: ", $nome, "<br> CPF: ", $cpf, "<br> RG: ", $rg;
+echo "Dados: <br> Nome: ", $nome, "<br> CPF: ", $cpf, "<br> RG: ", $rg, "<br> Sexo: ", $sexo;
 
 if($saldo<$totalCompra){
     echo "<br>Saldo Insuficiente para Concluir Compra!";
@@ -13,6 +14,4 @@ if($saldo<$totalCompra){
 else{
     echo "<br>Compra Concluída! Saldo disponível: ", $saldo - $totalCompra;
 }
-
-
 ?>
