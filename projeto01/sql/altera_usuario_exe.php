a6c3d1bf5bbc3fc28b278c2becfbae47b020b5f8
nataly <tinarellinataly@gmail.com>
2022-05-11 10:34:41 -0300

    alteração dados

---
+++ projeto01/sql/altera_usuario_exe.php
@@ -0,0 +1,24 @@
+<?php
+    include('conexao.php');
+    $id_usuario = $_POST['id_usuario'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+
+    echo "<h1> Alteração de dados </h1>";
+    echo "<p> Nome Usuário: " . $nome . "<p>";
+
+    $sql = "UPDATE usuario SET //atualização do banco de dados
+        nome_usuario='".$nome."'; //aspas duplas entre strings
+        email_usuario='".$email."'; 
+        telefone_usuario='".$telefone."'; 
+        WHERE id_usuario=".$id_usuario; 
+
+    $result = mysqli_query($con, $sql); //acessando o banco
+    if($result)
+        echo "Dados alterados com sucesso <br>";
+    else
+        echo "Erro ao alterar no banco de dados: ".mysqli_error($con)."<br>";
+
+?>
+<a href="index.php"> Voltar </a>
