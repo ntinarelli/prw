4d79c0aa28b55405ebc46223d81c2161f4caee0f
nataly <tinarellinataly@gmail.com>
2022-05-11 12:08:41 -0300

    banco de dados

---
+++ lista6_agenda/sql/agenda.sql
@@ -0,0 +1,22 @@
+--criar banco de dados
+create database lista6_agenda;
+
+--selecionar banco de dados
+use lista6_agenda;
+
+--criar tabela agenda
+create table agenda (
+    id_agenda int not null auto_increment,
+    nome varchar(50) not null,
+    apelido varchar(40) not null,
+    endereco varchar(70) not null,
+    bairro varchar(50) not null,
+    cidade varchar(50) not null,
+    estado varchar(02) not null,
+    telefone varchar(15) not null,
+    celular varchar(15) not null,
+    email varchar(70) not null,
+    dt_cadastro date not null,
+    primary key (id_agenda)
+)
+
