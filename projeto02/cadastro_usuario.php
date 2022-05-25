a451bf67901b639129aeb4cf4bebdd9c184df7e9
nataly <tinarellinataly@gmail.com>
2022-05-25 11:00:13 -0300

    inserindo arquivos

--- projeto02/cadastro_usuario.php
+++ projeto02/cadastro_usuario.php
@@ -1,5 +1,22 @@
 <?php
     include('conexao.php');
+    //upload da foto
+    
+    $fotoNome = $_FILES['foto']['name'];
+    $target_dir = "upload/";//onde jogo minhas fotos
+    $target_file = $target_dir . basename($_FILES["foto"]["name"]);//concatenar a foto com o nome do arquivo
+    //Selecionar tipo de arquivo
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    //verificar se o tipo de arquivo é permitido (pega o final do arquivo e verifica se é permitido)
+    $extensions_arr = array("jpg","jpeg","png","gif");
+    //checar extensões
+    if( in_array($imageFileType,$extensions_arr) ){        
+        // Upload file
+        if(move_uploaded_file($_FILES['foto']['tmp_name'],$target_dir.$fotoNome)){
+            $fotoBlob = addslashes(file_get_contents($target_dir.$fotoNome));
+        }
+    }
+
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
