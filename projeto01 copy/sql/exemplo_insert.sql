df1f7a4eb0f253918b0f75fc758fcbd22466138f
nataly <tinarellinataly@gmail.com>
2022-05-25 10:18:33 -0300

    projeto

--- projeto01 copy/sql/exemplo_insert.sql
+++
@@ -1,6 +0,0 @@
-USE projeto01;
-
-INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
-VALUES ('Murilo Sauro', 'murilo@mail.com', '9090-7070');
-
-SELECT * FROM usuario;
--- projeto01 copy/sql/usuario.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM projeto01.usuario;
