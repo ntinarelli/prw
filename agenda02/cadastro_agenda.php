23dc51a250a35f7b9c95a5dce14dcf5375103c00
nataly <tinarellinataly@gmail.com>
2022-06-01 09:31:02 -0300

    inserindo foto

--- agenda02/cadastro_agenda.php
+++ agenda02/cadastro_agenda.php
@@ -1,5 +1,21 @@
 <?php
     include('conexao.php');
+
+    $fotoNome = $_FILES['foto']['name'];
+    $target_dir = "upload/";//onde jogo minhas fotos
+    $target_file = $target_dir . basename($_FILES["foto"]["name"]);//concatenar a foto com o nome do arquivo
+    //Selecionar tipo de arquivo
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    //verificar se o tipo de arquivo é permitido (pega o final do arquivo e verifica se é permitido)
+    $extensions_arr = array("jpg","jpeg","png","gif");
+    //checar extensões
+    if( in_array($imageFileType,$extensions_arr) ){        
+        // Upload file
+        if(move_uploaded_file($_FILES['foto']['tmp_name'],$target_dir.$fotoNome)){
+            $fotoBlob = addslashes(file_get_contents($target_dir.$fotoNome));
+        }
+    }
+
     $nome = $_POST['nome'];
     $apelido = $_POST['apelido'];
     $endereco = $_POST['endereco'];
@@ -24,8 +40,8 @@
     echo "Email: " . $email . "</p>";
     echo "Data: " . $dt_cadastro . "</p>";
 	
-	$sql = "INSERT INTO agenda (nome, apelido, endereco, bairro, cidade, estado, telefone, celular, email) 
-	        VALUES ('".$nome."','".$apelido."','".$endereco."','".$bairro."','".$cidade."','".$estado."','".$telefone."','".$celular."','".$email."')";
+	$sql = "INSERT INTO agenda (nome, apelido, endereco, bairro, cidade, estado, telefone, celular, email, foto_blob, foto_nome) 
+	        VALUES ('".$nome."','".$apelido."','".$endereco."','".$bairro."','".$cidade."','".$estado."','".$telefone."','".$celular."','".$email."','".$fotoBlob."','".$fotoNome."')";
 	    
 	$result = mysqli_query($con, $sql);
 	if($result)
