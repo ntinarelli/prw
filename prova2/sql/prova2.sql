c4231bf1aa96e45f1d7d3a644eaf803d2d2567d6
nataly <tinarellinataly@gmail.com>
2022-06-08 10:04:07 -0300

    prova2

---
+++ prova2/sql/prova2.sql
@@ -0,0 +1,20 @@
+-- criar banco de dados
+create database prova2;
+
+-- selecionar banco de dados
+use prova2;
+
+drop table if exists fluxo_caixa;
+
+-- criar tabela fluxo_caixa
+create table fluxo_caixa 
+(
+    id        integer        not null auto_increment,
+    data      date           not null,
+    tipo      varchar(10)    not null,
+    valor     decimal(10, 2) not null,
+    historico varchar(150)   not null,
+    cheque    varchar(3)     not null,
+
+    primary key (id)
+);
